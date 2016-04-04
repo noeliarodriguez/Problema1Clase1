e2641c26eff1521b6739752235ff2d2a54541d0d
Noelia Rodriguez <rodrigueznoeliasabrina@yahoo.com>
2016-04-04 11:37:23 -0300

    Added files via upload

---
+++ holamundo1.cpp
@@ -0,0 +1,12 @@
+// holamundo1.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Hola mundo" << endl;
+	system("pause");
+	return 0;
+}
+
